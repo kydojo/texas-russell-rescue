a9250353e8afd0c8d1501dc3f00d1deb66373c25
kydojo <kyledjohnson@hotmail.com>
2019-07-13 17:32:43 -0500

    More Navbar work; it's still a WIP, but links all redirect to the correct page now, with filler contller content.

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from PIL import Image   # Python Imaging Library; for image resizing (Pillow)
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
 # import the classes from forms.py since it's in the same dir
-from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+# from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 
@@ -37,10 +37,45 @@ def index():
 def home():
     return render_template('home.html', title='Home', posts=posts)
 
-
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/volunteer")
+def volunteer():
+    return render_template('volunteer.html', title='Volunteer')
+
+@app.route("/before_you_adopt")
+def before_you_adopt():
+    return render_template('before_you_adopt.html', title='Before You Adopt')
+
+@app.route("/adoption_application")
+def adoption_application():
+    return render_template('adoption_app.html', title='Adoption Application')
+
+@app.route("/rescues")
+def rescues():
+    return render_template('rescues.html', title='Rescues')
+
+@app.route("/owner_listings")
+def owner_listings():
+    return render_template('owner_listings.html', title='Owner Listings')
+
+@app.route("/spotlight_terriers")
+def spotlight_terriers():
+    return render_template('spotlight_terriers.html', title='Spotlight Terriers')
+
+@app.route("/happy_tails")
+def happy_tails():
+    return render_template('happy_tails.html', title='Happy Tails')
+
+@app.route("/owner_listing_application")
+def owner_listing_application():
+    return render_template('owner_listing_app.html', title='Owner Listing Application')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title='Contact Us')
 
 
 @app.route("/register", methods=['GET', 'POST'])
