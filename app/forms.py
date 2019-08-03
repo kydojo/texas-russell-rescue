182dd7cfe13360e6d0e472cc2aeb7ca92aac97b4
Neal <quigleyn@oregonstate.edu>
2019-08-03 11:39:49 -0700

    finished adoption app route

--- app/forms.py
+++ app/forms.py
@@ -78,6 +78,7 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError(
                     'That email address is taken. Please choose another.')
 
+
 STATE_LIST = [
     ('TX', 'Texas'),
     ('AL', 'Alabama'),
@@ -142,10 +143,11 @@ class ContactUsForm(FlaskForm):
     content = TextAreaField('Message', validators=[DataRequired()])
     submit = SubmitField('Send')
 
+
 # TODO - Vaccination due date field needs to be added in concert with the vaccines_current field
-# TODO - Might want to replace some of the boolean checkboxes with radio fields
+# TODO - Need to replace the boolean checkboxes with radio fields
 # TODO - Need to add ability to upload at least 3 photos and 1 video
-# TODO - validation seems wonky. If I try to submit an empty form the notifications are not helpful
+# TODO - Validation seems wonky. If I try to submit an empty form the notifications are not helpful
 class OwnerSurrenderForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired(), Length(max=60)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(max=60)])
@@ -194,34 +196,63 @@ class OwnerSurrenderForm(FlaskForm):
     )
     submit = SubmitField('Send')
 
-class VolunteerApplicationForm(FlaskForm):
+
+MALE_FEMALE_LIST = [
+    ('MALE', 'Age'),
+    ('FEMALE', 'Sex'),
+    ('NO PREFERENCE', 'No Preference')
+]
+
+ALTERNATIVES_LIST = [
+    ('AGE', 'Age'),
+    ('SEX', 'Sex'),
+    ('NEITHER', 'Neither')
+]
+
+HOUSING_TYPE_LIST  = [
+    ('HOUSE', 'House'),
+    ('APARTMENT', 'Apartment'),
+    ('TRAILER', 'Trailer'),
+    ('TOWNHOME', 'Townhome'),
+    ('OTHER', 'Other')
+]
+
+CALL_TIME_LIST = [
+    ('MORNING', 'Morning'),
+    ('NOON', 'Noon'),
+    ('AFTERNOON', 'Afternoon'),
+    ('EVENING', 'Evening')
+]
+
+class AdoptionApplicationForm(FlaskForm):
     # Adoption Preferences
-    terrier_name
-    male_female
-    dog_age
-    willing_to_consider_alternatives
+    terrier_name = StringField('Name of terrier you would like to adopt (if unknown, type 'Unknown')', validators=[DataRequired(), Length(max=60)])
+    male_female = SelectField('State', choices=MALE_FEMALE_LIST, validators=[DataRequired()])
+    dog_age = StringField('Age Preference', validators=[DataRequired(), Length(max=60)])
+    willing_to_consider_alternative  = SelectField('I am willing to consider a suitable dog of a different', choices=ALTERNATIVES_LIST, validators=[DataRequired()])
 
     # Applicant Information
     first_name = StringField('First Name', validators=[DataRequired(), Length(max=60)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(max=60)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     home_phone = StringField('Home Phone', validators=[Length(max=20)])
-    cell_phone
-    work_phone 
-    best_time_to_call 
+    cell_phone = StringField('Cell Phone', validators=[Length(max=20)]) 
+    work_phone = StringField('Work Phone', validators=[Length(max=20)]) 
+    best_time_to_call = SelectField('Best time to Call', choices=CALL_TIME_LIST validators=[DataRequired()]) 
     street_address = StringField('Street Address', validators=[DataRequired(), Length(max=120)])
-    city = StringField('City', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired(), Length(max=60)])
     state = SelectField('State', choices=STATE_LIST, validators=[DataRequired()])
-    zip_code
-    occupation
+    zip_code = StringField('Zip', validators=[DataRequired()])
+    occupation = StringField('Occupation', validators=[DataRequired()])
     when_able_to_take_posession = TextAreaField(
         'If approved, when could you take possession of a dog?', validators=[DataRequired()]
     )
-    how_far_willing_to_travel
+    how_far_willing_to_travel = StringField
 
     # Housing/landlord Information
-    housing_type
-    rent_or_own
+    housing_type = SelectField('I (we) live in a:', choices=HOUSING_TYPE_LIST, validators=[DataRequired()])
+    housing_type_if_other = StringField('', validators=[Length(max=60)])
+    rent_or_own = StringField('', validators=[DataRequired()])
     landlord_permission
     landlord_name
     landlord_phone
